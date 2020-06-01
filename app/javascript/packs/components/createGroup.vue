d416e7ef8b5e76b0164cdb7468809bbcfe7607b3
Takaf19 <tk.f19ms@gmail.com>
2020-06-01 17:44:23 +0900

    create createGroup.vue

---
+++ app/javascript/packs/components/createGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="createGroup">
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "topPage",
+  components: {
+    newTab: NewTab,
+    popuTab: PopuTab,
+  },
+}
+
+</script>
+
+
+<style scoped>
+
+
+</style>
