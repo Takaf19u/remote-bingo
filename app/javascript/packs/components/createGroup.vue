aeaec1da6660414d6fa4444f992090431b6298e9
Takaf19 <tk.f19ms@gmail.com>
2020-06-01 22:52:15 +0900

    Update createGroup.vue

--- app/javascript/packs/components/createGroup.vue
+++ app/javascript/packs/components/createGroup.vue
@@ -1,23 +1,35 @@
 <template>
   <div id="createGroup">
-    
+    <h1>グループ情報を入力</h1>
+      <div class="inputBox">
+        <label for="master">主催者名</label>
+      </div>
+    <form>
+    </form>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: "topPage",
-  components: {
-    newTab: NewTab,
-    popuTab: PopuTab,
+  data() {
+    return {
+      form: {
+        masterName: '',
+        groupName: '',
+        pasword: '',
+      },
+    }
   },
+ 
 }
 
 </script>
 
 
 <style scoped>
-
+  #createGroup {
+    color: white;
+  }
 
 </style>
