cefeb8f5116454d1b390d1b7f39e312d9dd53c12
Takaf19 <tk.f19ms@gmail.com>
2020-06-03 14:28:43 +0900

    add helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,21 @@ module UsersHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # ユーザー情報の格納
+  def current_user
+    # ||= 自己代入
+    # 変数(右辺)の中身が、
+    # nilまたは、falseであれば、||=の右辺にある値を変数に代入する。
+    # nilまたは、false以外であれば、変数の値をそのまま利用する。
+    # @current_userが入っていない場合のみ、格納が行われるので、
+    # 処理の高速化ができる。
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
