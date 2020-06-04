ced96daa839c75301e4248989b9a25fa935f4075
Takaf19 <tk.f19ms@gmail.com>
2020-06-04 18:17:24 +0900

    Update rails route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'groups#index'
+  get '/createGroup', to: 'groups#index'
+  get '/users/sign_up', to: 'groups#index'
+
   # デバイスのルーティングをカスタマイズ
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-
-  root 'groups#index'
-
-  get '/createGroup', to: 'groups#index'
 
   resources :groups, only: [:index, :create] do
     collection do
