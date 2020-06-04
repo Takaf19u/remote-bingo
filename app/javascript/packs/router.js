604cfb83c55df727448f84cd21ec7f81e4d491d8
Takaf19 <tk.f19ms@gmail.com>
2020-06-04 23:07:02 +0900

    Update router

--- app/javascript/packs/router.js
+++ app/javascript/packs/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue/dist/vue.esm';
 import VueRouter from 'vue-router';
 import Top from './components/top.vue';
 import CreateGroup from './components/createGroup.vue';
+import UserForm from './components/form.vue';
 import UserNew from './components/user/userNew.vue';
 
 Vue.use(VueRouter);
@@ -11,7 +12,8 @@ const router = new VueRouter({
     routes: [
     { path: '/', component: Top },
     { path: '/createGroup', component: CreateGroup },
-    { path: '/users/sign_up', component: UserNew },
+    { path: '/users/sign_up', component: UserForm },
+    { path: '/users/sign_in', component: UserForm },
   ]
 });
 
