e224c7df8ef2191e1f7117c5c179a9e5e5899f3a
Takaf19 <tk.f19ms@gmail.com>
2020-06-05 23:32:28 +0900

    Update mainContents.vue

--- app/javascript/packs/components/mainContents.vue
+++ app/javascript/packs/components/mainContents.vue
@@ -2,14 +2,16 @@
     <div id="toppage">
       <div id="topItems">
         <div id="logobox">
+          <span class="cp_loading03 cp_item"> </span>
           <div class="logo">
             <b class="logotitle1">remo<span id="logo1">t</span>e</b>
             <b class="logotitle2"><span>B</span>I<span>N</span>GO</b>
           </div>
-          <span id="topbtn" @click="checkTopBtn">CLICK HERE</span>
         </div>
       </div>
       <div id="mainItems">
+        <div class="items-name">
+        </div>
         <div class="mainbtns">
           <router-link to="/createGroup" class="mainItems__btn mainbtn1">
             <div class="text text1">
@@ -35,23 +37,17 @@
   @import url(//fonts.googleapis.com/css?family=Vibur);
 
   #toppage {
-    height: 50%;
     width: 100%;
-    position: relative;
+    padding-top: 2%;
   }
   #topItems {
     display: flex;
     align-items: center;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-     right: 0;
-     left: 0;
     margin: auto;
-    height: 100%;
+    height: 80vh;
     width: 100%;
-    background-image: url("/assets/Brick-background.jpg");
-    background-size: auto;
+    /* background-image: url("/assets/Brick-background.jpg");
+    background-size: auto; */
     position: relative;
     z-index: 1;
 }
@@ -65,9 +61,9 @@
    left: 0;
   height: 100%;
   width: 100%;
-  background: rgba(22, 22, 22, 0.537);
-  background-image: -webkit-radial-gradient(rgba(218, 214, 214, 0.253),(82, 81, 81), rgba(14, 13, 13, 0.651));
-  background-image: radial-gradient(rgba(218, 214, 214, 0.253), rgba(12, 11, 11, 0.651));
+  background: rgba(49, 49, 49, 0.537);
+  background-image: -webkit-radial-gradient(rgba(48, 47, 47, 0.253), black);
+  background-image: radial-gradient(rgba(48, 47, 47, 0.253), black);
 }
 
   #logobox {
@@ -75,51 +71,30 @@
     user-select: none;
     margin: 10px auto;
     position: relative;
-    width: 400px;
     z-index: 2;
   }
   
   .logo {
     position: relative;
-    padding: 10px 20px;
   }
-  .logo::after {
-    content: "";
-    position: absolute;
-     top: 0;
-     bottom: 0;
-     right: 0;
-     left: 0;
-    height: 100%;
-    width: 100%;
-
-    color: rgba(8, 221, 221, 0.6);
-    border: 2px solid rgba(8, 221, 221, 0.6);
-    border-radius: 4px;
-    background-color: transparent;
-    box-shadow: 0px 0px 5px 5px rgba(8, 221, 221, 0.6);
-    animation: flickering 5s infinite;
-  }
-
-
 
   .logo b{
     color: rgba(8, 221, 221, 0.6);
-    text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em rgba(8, 221, 221, 0.6), 0 0 0.5em rgba(8, 221, 221, 0.6), 0 0 0.1em rgba(8, 221, 221, 0.6), 0 10px 3px #000;
+    text-shadow: 0 -40px 80px, 0 0 2px, 0 0 1em rgba(8, 221, 221, 0.6), 0 0 0.5em rgba(8, 221, 221, 0.6), 0 0 0.1em rgba(8, 221, 221, 0.6), 0 10px 3px #000;
     display: block;
     line-height: 1;
     margin: 10px;
   }
 
   .logotitle1 {
-    font: 400 12vh "Vibur";
+    font: 400 20vh "Vibur";
   }
 
 .logotitle2 {
-    font: 400 18vh "Vibur";
+    font: 400 25vh "Vibur";
 }
 
-  #logo1{
+  /* #logo1{
     animation: blink linear infinite 2s;
   }
 
@@ -128,7 +103,7 @@
 }
   .logotitle2 span:nth-child(2) {
     animation: blink2 linear infinite 3.5s;
-}
+} */
 
   #topbtn {
     display: inline-block;
@@ -177,11 +152,8 @@
   }
 
   #mainItems {
-    position: absolute;
-    bottom: 0;
     height: 50%;
     width: 100%;
-    box-shadow: 0 8px 8px -3px gray inset;
     align-items: center;
     padding: 20px;
   }
@@ -252,122 +224,110 @@
   }
 
 
-/* ロゴの処理 */
-  @keyframes flicker {
-    50% {
-      color: white;
-      filter: saturate(200%) hue-rotate(20deg);
-    }
-  }
-
-  @keyframes blink {
-    78% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    79%{
-      color: #333;
-    }
-    80% {
-      
-      text-shadow: none;
-    }
-    81% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    82% {
-      color: #333;
-      text-shadow: none;
-    }
-    83% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    92% {
-      color: #333;
-      text-shadow: none;
-    }
-    92.5% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-  }
-  
-  @keyframes blink2 {
-    78% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    79%{
-      color: rgb(222, 226, 4);
-    }
-    80% {
-      
-      text-shadow: none;
-    }
-    81% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    82% {
-      color: rgb(10, 135, 252);
-      text-shadow: none;
-    }
-    83% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    92% {
-      color: rgba(241, 10, 222, 0.5);
-      text-shadow: none;
-    }
-    92.5% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-  }
-
-  @keyframes flickering {
-    0% {
-      border-color: transparent;
-      opacity: 0.2;
-    }
-    
-    2% {
-    color: rgba(8, 221, 221, 0.6);
-      opacity: 1;
-    }
-    
-    4% {
-      border-color: transparent;
-      opacity: 0.2;
-    }
-    
-    8% {
-    color: rgba(8, 221, 221, 0.6);
-      opacity: 1;
-    }
-    
-    28% {
-    color: rgba(8, 221, 221, 0.6);
-      opacity: 1;
-    }
-    
-    30% {
-      border-color: transparent;
-      opacity: 0.2;
-    }
-    
-    36% {
-    color: rgba(8, 221, 221, 0.6);
-      opacity: 1;
-    }
-    
-    100% {
-    color: rgba(8, 221, 221, 0.6);
-      opacity: 1;
-    }
-  }
+/* ぐるぐるする処理 */
+.cp_loading03 {
+	position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+	width: 80vh;
+	height: 80vh;
+}
+.cp_loading03.cp_item {
+	-webkit-animation: loading03 10s linear infinite;
+	        animation: loading03 10s linear infinite;
+	border: 10px solid transparent;
+	border-top-color: #f06292;
+	border-bottom-color: #f06292;
+	border-radius: 80vh;
+}
+.cp_loading03.cp_item:before,
+.cp_loading03.cp_item:after {
+	position: absolute;
+	top: 50px;
+	right: 50px;
+	bottom: 50px;
+	left: 50px;
+	content: '';
+	-webkit-animation: loading03_reverse 8s linear infinite;
+	        animation: loading03_reverse 8s linear infinite;
+	opacity: .6;
+	border: 5px solid transparent;
+	border-top-color: #f06292;
+	border-bottom-color: #f06292;
+	border-radius: 80vh;
+	filter: alpha(opacity=60);
+}
+.cp_loading03.cp_item:before {
+	top: 100px;
+	right: 100px;
+	bottom: 100px;
+	left: 100px;
+	-webkit-animation: loading03_reverse 5s cubic-bezier(.17,.67,.88,.52) infinite;
+	        animation: loading03_reverse 5s cubic-bezier(.17,.67,.88,.52) infinite;
+	opacity: .8;
+	border: 6px solid transparent;
+	border-top-color: #f06292;
+	border-bottom-color: #f06292;
+	filter: alpha(opacity=80);
+}
+@-webkit-keyframes loading03 {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	50% {
+		-webkit-transform: rotate(180deg);
+		transform: rotate(180deg);
+	}
+	100% {
+		-webkit-transform: rotate(360deg);
+		transform: rotate(360deg);
+	}
+}
+@keyframes loading03 {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	50% {
+		-webkit-transform: rotate(180deg);
+		transform: rotate(180deg);
+	}
+	100% {
+		-webkit-transform: rotate(360deg);
+		transform: rotate(360deg);
+	}
+}
+@-webkit-keyframes loading03_reverse {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	50% {
+		-webkit-transform: rotate(180deg);
+		transform: rotate(180deg);
+	}
+	100% {
+		-webkit-transform: rotate(-360deg);
+		transform: rotate(-360deg);
+	}
+}
+@keyframes loading03_reverse {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	50% {
+		-webkit-transform: rotate(180deg);
+		transform: rotate(180deg);
+	}
+	100% {
+		-webkit-transform: rotate(-360deg);
+		transform: rotate(-360deg);
+	}
+}
 
 </style>
