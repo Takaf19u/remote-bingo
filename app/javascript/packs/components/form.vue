6df40e99cc11914786a4a8c6a8aa72d8a2904cbd
Takaf19 <tk.f19ms@gmail.com>
2020-06-09 23:31:18 +0900

    Update form.vue

--- app/javascript/packs/components/form.vue
+++ app/javascript/packs/components/form.vue
@@ -2,9 +2,11 @@
   <div id="userForm">
     <div class="bar" id="left-bar"></div>
     <div class="bar" id="right-bar"></div>
-    <transition mode="out-in" name="slide">
-      <div v-bind:is="component"></div>
-    </transition>
+    <div id="changeForm">
+      <transition mode="out-in" name="slide">
+        <div v-bind:is="component"></div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -45,7 +47,11 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  #userForm {
+    height: 100%;
+    overflow: scroll;
+  }
 
   .bar {
     height: 100%;
@@ -58,6 +64,7 @@ export default {
       top: 0;
       bottom: 0;
       left: 5%;
+    height: 100%;
   }
 
   #right-bar {
@@ -65,6 +72,7 @@ export default {
       top: 0;
       bottom: 0;
       right: 5%;
+    height: 100%;
   }
 
   #left-bar::after {
@@ -89,7 +97,105 @@ export default {
     background: linear-gradient(30deg, #bd6cff, #6cffeb, #e8f80c);
   }
 
-  form {
-    color: white;
+  #changeForm {
+    width: 450px;
+    height: 100%;
+    margin: 0 auto;
+    padding-top: 5vh;
   }
+  
+  /deep/ form {
+    & h1 {
+      font-family: 'Monoton', cursive;
+      font-size: 3.5em;
+      text-align: center;
+      margin-bottom: 50px;
+      font-weight: 600;
+    }
+    .inputBox {
+      width: 100%;
+      margin-bottom: 8vh;
+      position: relative;
+      & label {
+        position: absolute;
+          top: -15px;
+          left: 5px;
+        color: rgb(0, 110, 255);
+        font-size: 1em;
+        background: black;
+        padding: 0 2px;
+      }
+      & input {
+        width: 100%;
+        padding: 5px 8px;
+        color: white;
+        font-size: 2.8vh;
+        font-weight: 200;
+        border: solid 2px rgb(0, 110, 255);
+        border-radius: 5px;
+        outline: none;
+        background: transparent;
+        box-shadow: 0px 0px 0px 1px rgb(129, 128, 128);
+        /* placeholderの色 */
+        &::placeholder {
+          font-size: 0.7em;
+          color: rgba(177, 175, 175, 0.897);
+        }
+        /* IE */
+        &:-ms-input-placeholder {
+          font-size: 0.7em;
+          color: rgba(177, 175, 175, 0.897);
+        }
+        /* Edge */
+        &::-ms-input-placeholder {
+          font-size: 0.7em;
+          color: rgba(177, 175, 175, 0.897);
+        }
+      }
+    }
+    & .formbtn {
+      width: 100%;
+      margin: 8vh auto 0;
+      display: flex;
+      justify-content: center;
+      padding: 0 10px;
+      &--item {
+        margin: 0 30px;
+        font-family: 'Montserrat', sans-serif;
+        text-decoration: none;
+        font-size: 1.6em;
+        font-weight: 200;
+        display: inline-block;
+        background: #f36f6fb9;
+        color: rgb(194, 194, 194);
+        width: 100px;
+        height: 100px;
+        line-height: 100px;
+        border-radius: 50%;
+        text-align: center;
+        overflow: hidden;
+        box-shadow: 0px 2px 5px rgba(202, 248, 250, 0.59);
+        border-bottom: solid 3px #bd6565;
+        transition: .2s;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
+          padding: 0;
+          border: none;
+          outline: none;
+        &:hover {
+          background: #fa8b8b;
+          color: white;
+        }
+        &:active {
+          -webkit-transform: translateY(2px);
+          transform: translateY(2px);
+          box-shadow: 0 0 1px rgba(163, 231, 252, 0.5);
+          border-bottom: none;
+        }
+      }
+    }
+  }
+
+
 </style>
