22999256bb20c7204a2cab53ad0fd32376529833
Takaf19 <tk.f19ms@gmail.com>
2020-06-09 23:30:09 +0900

    Update controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,23 +5,21 @@ class GroupsController < ApplicationController
   end
 
   def create
-    master = User.new(user_params)
-    master.save
     binding.pry
     group = Group.new(group_params)
-    group.save
-    render 'create', formats: 'json', handlers: 'jbuilder'
+    if group.save
+      flash[:notice] = "グループを作成しました。"
+      render json: true
+    else
+      flash.now[:alert] = 'グループの作成に失敗しました。'
+      render json: false
+    end
   end
 
   private 
-  def user_params
-    # 子要素のstrong parameterを書くことで、自動で子要素に親要素のidもふられる。explanatinテーブルにあるrecreation_idに親要素のidがふられます。
-    params.require(:users).permit(:masterName)
-  end
-
-  def rec_params
+  def group_params
       # 子要素のstrong parameterを書くことで、自動で子要素に親要素のidもふられる。explanatinテーブルにあるrecreation_idに親要素のidがふられます。
-      params.require(:groups).permit(:groupName, :password ).merge(user_id: master.id)
+      params.require(:groups).permit(:groupName, :password ).merge(user_id: current_user.id)
   end
 
 end
