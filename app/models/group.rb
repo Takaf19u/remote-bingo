346a0ec795d0335f8d512d727e2957a46842c75e
Takaf19 <tk.f19ms@gmail.com>
2020-06-09 23:30:25 +0900

    add validate

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ApplicationRecord
+  has_secure_password
+  belongs_to :user
+
+  validates :groupName, presence: true, uniqueness: true, length: { minimum: 5 } 
+  validates :password_digest, presence: true, length: { in: 6..20 } 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups
+
+  validates :name, uniqueness: true, length: { minimum: 3 }
+  validates :name,:email, :encrypted_password, presence: true
 end
