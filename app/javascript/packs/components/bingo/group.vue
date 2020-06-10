823f7da2bda514523f7eaba6ea54753a1d9165d5
Takaf19 <tk.f19ms@gmail.com>
2020-06-10 23:29:16 +0900

    create group.vue

---
+++ app/javascript/packs/components/bingo/group.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="group">
+    <card></card>
+  </div> 
+</template>
+
+<script>
+import axios from "axios"
+import Card from "./card.vue"
+
+export default {
+  components: {
+    card: Card
+  },
+  methods: {
+  }
+ 
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
