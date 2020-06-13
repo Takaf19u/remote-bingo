f26505db3861c8d3a80feef121f2e68e0e1a7838
Takaf19 <tk.f19ms@gmail.com>
2020-06-13 22:30:36 +0900

    Update model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,8 @@
 class Group < ApplicationRecord
   has_secure_password
   belongs_to :user
+  has_many :cards
+  has_many :users, through: :cards
 
   validates :groupName, presence: true, uniqueness: true, length: { minimum: 5 } 
   validates :password_digest, presence: true, length: { minimum: 6 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :groups
+  has_many :cards
+  has_many :groups, through: :cards
 
   validates :name, uniqueness: true, length: { minimum: 3 }
   validates :name,:email, :encrypted_password, presence: true
