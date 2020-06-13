9f8e3b625bf607a48d781039d0ed37fa8fe2508e
Takaf19 <tk.f19ms@gmail.com>
2020-06-13 22:32:50 +0900

    create cards_controller

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,30 @@
+class CardsController < ApplicationController
+
+  def show
+    card = Card.find_by( group_id: params[:id], user_id: current_user.id )
+
+    respond_to do |format|
+      format.json {render json: {res_number: card.rand_number} }
+    end
+  end 
+
+  def create
+    card = Card.new(card_params)
+    if card.save
+      flash[:notice] = "グループを作成しました。"
+      url = "/createGroup/#{card.group_id}"
+    else
+      flash.now[:alert] = 'グループの作成に失敗しました。'
+      url = "/createGroup"
+    end
+    
+    respond_to do |format|
+      format.json {render json: url }
+    end
+  end
+
+  private 
+  def card_params
+      params.require(:cards).permit(:rand_number, :create_count, :group_id ).merge(user_id: current_user.id)
+  end
+end
